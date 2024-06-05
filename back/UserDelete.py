1ad8fcd4c2b4a496f4ef896b9c6f392406611683
J-sh1 <j2000star@kakao.com>
2024-06-05 12:48:13 +0900

    승혁 - 회원탈퇴 페이지 삭제, 회원탈퇴기능 완

--- back/UserDelete.py
+++ back/UserDelete.py
@@ -3,7 +3,21 @@ from db import db_con
 
 UserDelete_bp = Blueprint('UserDelete', __name__)
 
+# 회원탈퇴기능 나중에 회원정보 수정하는 백에 옮겨야함!!!!
 @UserDelete_bp.route('/UserDelete', methods = ['POST'])
 def UserDelete():
+    user_id = request.json.get('userId')
     data = request.get_json()
     db = db_con()
+    cursor = db.cursor()
+    try :
+        update_sql = "UPDATE TB_USER SET USER_STATUS = 'out' WHERE = %s"
+        cursor.execute(update_sql, (user_id))
+        db.commit()
+        return jsonify({"message : " : "회원탈퇴 완료"})
+    except Exception as e :
+        db.rollback()
+        return jsonify({"error" : str(e)})
+    finally:
+        cursor.close()
+        db.close()
