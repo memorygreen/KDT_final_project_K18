41155f114c4ce5072d141fefce12ce12119816e7
memorygreen <setterdayz0720@naver.com>
2024-06-13 12:43:26 +0900

    자영 커밋 240613 12:43

--- front/src/Page/MyPage/My_modify/UserUpdate.js
+++ front/src/Page/MyPage/My_modify/UserUpdate.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-// import './UserUpdate.css'; 
+import './UserUpdate.css'; 
 
 function UserUpdate() {
   const [userId, setUserId] = useState('');
