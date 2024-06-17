3029b80bd7bd018e1dae1fb00bc4c5f577205016
J-sh1 <j2000star@kakao.com>
2024-06-17 11:16:43 +0900

    승혁 - 실종자 등록 이미지 버그 수정

--- front/src/Components/SearchMissing/SearchMissing.js
+++ front/src/Components/SearchMissing/SearchMissing.js
@@ -5,22 +5,17 @@ import SearchBar from './SearchBar';
 import UploadMissingImg from './UploadMissingImg';
 import { createPoster } from '../Poster/CreatePost';
 import { MissingAvatar } from './MissingAvatar';
-import { useNavigate } from 'react-router-dom'; // useHistory 훅 추가
+import { useNavigate } from 'react-router-dom';
 
 import './SearchMissing.css';
 
 const SearchMissing = ({ initialData }) => {
-
     const navigate = useNavigate(); // 리다이렉트를 위한 navigate 함수 사용
-
 
     const sessionId = sessionStorage.getItem('userId') // session에 있는 id 값 
 
     const [selectedTxt, setSelectTxt] = useState('');
     const [selectBox, setSelectBox] = useState('');
-
-    // console.log('SearchMissing 컴포넌트에서 초기값 있는지 확인 data:', initialData ? initialData : 'No initial data')
-
 
     // 인적사항 변수
     const [missingName, setMissingName] = useState('');
@@ -40,9 +35,10 @@ const SearchMissing = ({ initialData }) => {
 
     const [missingClothesEtc, setMissingClothesEtc] = useState('');
     const [missingBelongingsEtc, setMissingBelongingsEtc] = useState('');
-    // 실종자 이미지 
-    const [missingImg, setMissingImg] = useState([]);
 
+    // 실종자 이미지 
+    const [missingImg, setMissingImg] = useState(null); // 기본 값을 null로 설정
+    const [isUploading, setIsUploading] = useState(false); // 업로드 상태 관리 추가
 
     // 인상착의의 라벨(한글)을 받기 위한 변수 설정
     const [selectedTopKor, setSelectedTopKor] = useState('');
@@ -51,14 +47,10 @@ const SearchMissing = ({ initialData }) => {
     const [selectedBottomColorKor, setSelectedBottomColorKor] = useState('');
     const [selectedBelongingsKor, setSelectedBelongingsKor] = useState('');
 
-
-
-
     /** 자영(240603):주소 받아오는 함수 */
     const getMissingLocation = (address) => {
         console.log('정상적으로 넘어왔습니다. getMissingLocation(address)', address);
         setMissingLocation(address);
-
     };
 
     /** 위도, 경도를 받아오는 함수 */
@@ -66,48 +58,47 @@ const SearchMissing = ({ initialData }) => {
         console.log('정상적으로 넘어왔습니다. getLatLon function', lat, lng);
         setMissingLocationLat(lat);
         setMissingLocationLng(lng);
-
     };
 
     const topOptions = [
-        { id: 'top long', label: '긴팔' }, //임시
-        { id: 'top short', label: '반팔' }, // 임시
+        { id: 'top long', label: '긴팔' },
+        { id: 'top short', label: '반팔' },
     ];
 
     const topColorOptions = [
-        { id: 'top red', label: '빨간색' }, // 변경 완
-        { id: 'top yellow', label: '노란색' }, // 변경 완
-        { id: 'top green', label: '초록색' }, //변경 완
-        { id: 'top blue', label: '파란색' }, //변경 완
-        { id: 'top purple', label: '보라색' }, //변경 완 추가
-        { id: 'top white', label: '흰색' }, // 변경완
-        { id: 'top gray', label: '회색' }, // 변경완
-        { id: 'top black', label: '검정색' }, //변경완
+        { id: 'top red', label: '빨간색' },
+        { id: 'top yellow', label: '노란색' },
+        { id: 'top green', label: '초록색' },
+        { id: 'top blue', label: '파란색' },
+        { id: 'top purple', label: '보라색' },
+        { id: 'top white', label: '흰색' },
+        { id: 'top gray', label: '회색' },
+        { id: 'top black', label: '검정색' },
     ];
 
     const bottomOptions = [
-        { id: 'bottom long', label: '긴바지' }, // 임시
-        { id: 'bottom short', label: '반바지' }, // 임시
-        { id: 'bottom skirt', label: '치마' }, // 변경완
+        { id: 'bottom long', label: '긴바지' },
+        { id: 'bottom short', label: '반바지' },
+        { id: 'bottom skirt', label: '치마' },
     ];
 
     const bottomColorOptions = [
-        { id: 'bottom yellow', label: '노란색' }, // 변경 완
-        { id: 'bottom green', label: '초록색' }, // 변경완
-        { id: 'bottom blue', label: '파란색' }, // 변경완
-        { id: 'bottom purple', label: '보라색' }, // 변경완
-        { id: 'bottom pink', label: '분홍색' }, // 변경완
-        { id: 'bottom brown', label: '갈색' }, // 변경완
-        { id: 'bottom white', label: '흰색' }, // 변경완
-        { id: 'bottom gray', label: '회색' }, // 변경완
-        { id: 'bottom black', label: '검정색' }, // 변경완
+        { id: 'bottom yellow', label: '노란색' },
+        { id: 'bottom green', label: '초록색' },
+        { id: 'bottom blue', label: '파란색' },
+        { id: 'bottom purple', label: '보라색' },
+        { id: 'bottom pink', label: '분홍색' },
+        { id: 'bottom brown', label: '갈색' },
+        { id: 'bottom white', label: '흰색' },
+        { id: 'bottom gray', label: '회색' },
+        { id: 'bottom black', label: '검정색' },
     ];
 
     const belongingsOptions = [
-        { id: 'hat', label: '모자' }, // 변경완
-        { id: 'backpack', label: '배낭' }, // 변경완
-        { id: 'shoulder bag', label: '숄더백' }, // 변경완
-        { id: 'hand bag', label: '핸드백' }, // 변경완
+        { id: 'hat', label: '모자' },
+        { id: 'backpack', label: '배낭' },
+        { id: 'shoulder bag', label: '숄더백' },
+        { id: 'hand bag', label: '핸드백' },
         { id: 'acc_none', label: '해당없음' },
     ];
 
@@ -117,19 +108,25 @@ const SearchMissing = ({ initialData }) => {
     ];
 
     // 포스터 생성기능
-    const [posterGenerating, setPosterGenerating] = useState('');
+    const [posterGenerating, setPosterGenerating] = useState(false); // 기본 값을 false로 설정
 
     const handle_submit = async (event) => {
         event.preventDefault();
+        let uploadedImageUrl = ''; // 변수를 함수 범위 내에서 정의
+
         if (missingImg) {
             try {
-                const uploadedImageUrl = await UploadMissingImg(missingImg); // 업로드 완료될때까지 기다리기
+                setIsUploading(true); // 업로드 상태 시작
+                uploadedImageUrl = await UploadMissingImg(missingImg); // 업로드 완료될때까지 기다리기
                 setMissingImgUrl(uploadedImageUrl);
                 console.log("업로드된 이미지 URL:", uploadedImageUrl);
-                // missingImgUrl = Url;
-                console.log("업로드된 이미지 URL (확인)):", missingImgUrl); // URL을 로그로 출력
+                // setMissingImgUrl을 제대로 업데이트한 후 그 값을 로그로 출력
+                console.log("업로드된 이미지 URL (확인):", uploadedImageUrl); 
             } catch (error) {
                 console.error('실종자 이미지 aws 업로드 실패 Failed to upload image');
+                return; // 업로드에 실패하면 이후 코드를 실행하지 않도록 합니다.
+            } finally {
+                setIsUploading(false); // 업로드 상태 종료
             }
 
             const prompt = `
@@ -145,10 +142,9 @@ const SearchMissing = ({ initialData }) => {
             `;
 
             console.log(prompt);
-            if (missingImgUrl) {
-                // 백으로 보내기
-                // POST request to submit form data
-                console.log('post 들어왔는지 확인')
+
+            // missingImgUrl이 설정된 후에만 백엔드로 데이터 전송
+            if (uploadedImageUrl) {
                 axios.post('/SearchMissing', {
                     session_id: sessionId,
 
@@ -159,7 +155,7 @@ const SearchMissing = ({ initialData }) => {
                     missing_location: missingLocation,
                     missing_location_lat: missingLocationLat,
                     missing_location_lng: missingLocationLng,
-                    missing_img_url: missingImgUrl,
+                    missing_img_url: uploadedImageUrl,
 
                     selected_top: selectedTop,
                     selected_top_color: selectedTopColor,
@@ -181,7 +177,6 @@ const SearchMissing = ({ initialData }) => {
                         alert("등록 성공(포스터 생성 시 완료까지 시간이 소요됩니다)")
                         navigate('/Map'); // 성공 후 메인 페이지로 리다이렉트
 
-
                         // 포스터 생성
                         if (posterGenerating) {
                             try {
@@ -189,31 +184,20 @@ const SearchMissing = ({ initialData }) => {
                                     await createPoster(prompt); // createPoster 함수 실행
                                 }, 5000);  //db값 저장되고 실행하도록 시간텀을 줌
                             } catch (error) {
-                                // createPoster 함수 실행 중 오류가 발생한 경우 처리
                                 console.error('Error creating poster:', error);
                             }
                         }
-
-                        
-
                     })
                     .catch(error => {
                         console.error('실종자 정보 등록 실패 에러 Error submitting report:', error);
                         alert("등록 실패")
-                        // Handle error
                     });
             } else {
                 console.error('실종자 이미지 url 받은 후 백으로 보내기 실패 No image to upload');
                 console.error('실종자 이미지 url을 업로드해주세요.');
             }
-
-
         }
-
-        // 포스터 생성
-
     };
-
 
     // 각 옵션의 label 값을 찾아서 상태에 저장하는 함수들
     const handleTopChange = (event) => {
@@ -246,7 +230,6 @@ const SearchMissing = ({ initialData }) => {
         setSelectedBelongingsKor(selectedOption ? selectedOption.label : '');
     };
 
-
     // 인적사항 변경
     const handleNameChange = (event) => {
         setMissingName(event.target.value);
@@ -263,9 +246,6 @@ const SearchMissing = ({ initialData }) => {
         setMissingBelongingsEtc(event.target.value);
     };
 
-    // const handleLocationChange = (event) => {
-    //     setMissingLocation(event.target.value);
-    // };
     const handleImgChange = (event) => {
         setMissingImg(event.target.files[0]);
     };
@@ -284,7 +264,6 @@ const SearchMissing = ({ initialData }) => {
             <div className="search_missing_cate_group">
                 <div className="search_missing_cate_content">
                     <h2>실종자 이름</h2>
-
                     <input
                         type="text"
                         className="input_etc"
@@ -294,7 +273,6 @@ const SearchMissing = ({ initialData }) => {
                         onChange={handleNameChange}
                         placeholder="실종자 이름 입력"
                     />
-
                 </div>
 
                 <div className="search_missing_cate_content">
@@ -348,9 +326,8 @@ const SearchMissing = ({ initialData }) => {
                             aria-label="Sizing example input"
                             aria-describedby="missing_img"
                             value={missingImg ? missingImg.name : '선택된 파일이 없습니다.'}
-                            onChange={handleNameChange}
                             placeholder={missingImg ? missingImg.name : '선택된 파일이 없습니다.'}
-                            readOnly='true'
+                            readOnly={true}
                         />
                         <input
                             type="file"
@@ -358,11 +335,13 @@ const SearchMissing = ({ initialData }) => {
                             id="missing_img"
                             onChange={handleImgChange}
                         />
+                        <button onClick={handle_submit} disabled={isUploading}>
+                            {isUploading ? '업로드 중...' : '이미지 업로드'}
+                        </button>
                     </div>
                     {/** 미리보기 */}
                     {missingImg && (
                         <div>
-                            {/* <div className="uploaded-file-name">업로드된 파일: {missingImg.name}</div> */}
                             {missingImg instanceof File && (
                                 <img src={URL.createObjectURL(missingImg)} alt="Uploaded" style={{ width: '100px', height: '100px' }} />
                             )}
@@ -372,7 +351,6 @@ const SearchMissing = ({ initialData }) => {
 
                 <div className="search_missing_cate_content">
                     <h2>포스터 생성 유무 체크</h2>
-
                     <div className='search_missing_cate_content_poster'>
                         <input
                             type="checkbox"
@@ -382,7 +360,7 @@ const SearchMissing = ({ initialData }) => {
                             checked={posterGenerating} // 이 상태는 useState를 사용하여 관리해야 합니다.
                             onChange={(event) => setPosterGenerating(event.target.checked)}
                         />
-                        <label className="radio_btn_label_poster" htmlFor='poster_check_box' >포스터 생성</label>
+                        <label className="radio_btn_label_poster" htmlFor='poster_check_box'>포스터 생성</label>
                         <input
                             type="text"
                             className="input_etc_poster_notice"
@@ -391,7 +369,7 @@ const SearchMissing = ({ initialData }) => {
                             value={posterGenerating ? '실종자인상착의 검색&실종자 찾기 포스터 생성' : '실종자인상착의 검색만 실행'}
                             onChange={handleNameChange}
                             placeholder={posterGenerating ? '실종자인상착의 검색&실종자 찾기 포스터 생성' : '실종자인상착의 검색만 실행'}
-                            readOnly='true'
+                            readOnly={true}
                         />
                     </div>
                 </div>
@@ -420,7 +398,6 @@ const SearchMissing = ({ initialData }) => {
                                 <label className="radio_btn_label" htmlFor={option.id}>{option.label}</label>
                             </React.Fragment>
                         ))}
-
                     </div>
                 </div>
 
@@ -444,9 +421,6 @@ const SearchMissing = ({ initialData }) => {
                         ))}
                     </div>
                 </div>
-
-
-
             </div>
         );
     };
@@ -457,10 +431,8 @@ const SearchMissing = ({ initialData }) => {
                 <div className="search_missing_cate_content">
                     <h2>하의 구분</h2>
                     <div className='search_missing_cate_content_buttons'>
-
                         {bottomOptions.map(option => (
                             <React.Fragment key={option.id}>
-
                                 <input
                                     type="radio"
                                     className="radio_btn"
@@ -474,8 +446,6 @@ const SearchMissing = ({ initialData }) => {
                                 <label className="radio_btn_label" htmlFor={option.id}>{option.label}</label>
                             </React.Fragment>
                         ))}
-
-
                     </div>
                 </div>
 
@@ -500,7 +470,6 @@ const SearchMissing = ({ initialData }) => {
                     </div>
                 </div>
 
-
                 <div className="search_missing_cate_content">
                     <h2>인상착의 특이사항</h2>
                     <input
@@ -513,7 +482,6 @@ const SearchMissing = ({ initialData }) => {
                         placeholder="인상착의 특이사항 입력"
                     />
                 </div>
-
             </div>
         );
     };
@@ -544,7 +512,6 @@ const SearchMissing = ({ initialData }) => {
 
                 <div className="search_missing_cate_content">
                     <h2>소지품 특이사항</h2>
-
                     <input
                         type="text"
                         className="input_etc"
@@ -555,7 +522,6 @@ const SearchMissing = ({ initialData }) => {
                         placeholder="소지품 특이사항 입력"
                     />
                 </div>
-
             </div>
         );
     };
@@ -572,12 +538,11 @@ const SearchMissing = ({ initialData }) => {
         selectedComponent = missing_belongings_box();
     }
 
-
     return (
         <div className='SearchMissing_all'>
             <div className='search_bar_all'>
                 <div className='search_missing_avatar_all'>
-                    < MissingAvatar
+                    <MissingAvatar
                         selectedTop={selectedTop}
                         selectedTopColor={selectedTopColor}
                         selectedBottom={selectedBottom}
@@ -589,7 +554,6 @@ const SearchMissing = ({ initialData }) => {
                         {selectedComponent}
                     </div>
                 </div>
-
             </div>
         </div>
     );
--- front/src/Page/SearchMissingPage/SearchMissingPage.js
+++ front/src/Page/SearchMissingPage/SearchMissingPage.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import NevBar from '../../Components/NevBar/NevBar';
-import SearchMissing from '../../Components/SearchMissing/SearchMissing'
+import SearchMissing from '../../Components/SearchMissing/SearchMissing';
+
 const SearchMissingPage = () => {
     return (
-
         <div className="search_missing_page">
             <header className='nevibar_card'> <NevBar /></header> 
-            <div className='Main_card' >
-                <SearchMissing initialData={ null }/>
+            <div className='Main_card'>
+                <SearchMissing initialData={null} />
             </div>
         </div>
-
-        
     );
 };
 
